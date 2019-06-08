f19d98bae871d2091fff0b7027543ab98ac4d117
divisor <div@altertech.com>
2019-06-08 02:10:05 +0200

    deploy fix

--- .online/_online-deploy.sh
+++ .online/_online-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ./deploy.sh docker-compose-online-demo.yml || exit 1
-docker exec -t eva_farm_scada eva sfa server stop || exit 1
+docker exec -t eva_smarthome_server eva sfa server stop || exit 1
 if [ ! -f ./data/sfa_history.db ]; then
-  docker exec eva_farm_scada sh -c 'mv /opt/eva/runtime/db/sfa_history.db /data/' || exit 1
+  docker exec eva_smarthome_server sh -c 'mv /opt/eva/runtime/db/sfa_history.db /data/' || exit 1
 fi
-docker exec eva_farm_scada sh -c 'ln -sf /data/sfa_history.db /opt/eva/runtime/db/sfa_history.db' || exit 1
-docker exec -t eva_farm_scada eva sfa server start || exit 1
+docker exec eva_smarthome_server sh -c 'ln -sf /data/sfa_history.db /opt/eva/runtime/db/sfa_history.db' || exit 1
+docker exec -t eva_smarthome_server eva sfa server start || exit 1
 echo "-----------------------------------------------------------------"
 echo "Completed"
 exit 0
