b85778f3fb933c6b9ef8ba2ee98b50d60027b009
divisor <div@altertech.com>
2019-06-11 22:21:06 +0200

    cctv autostart

--- cfg/cctvsim/setup.sh
+++ cfg/cctvsim/setup.sh
@@ -3,6 +3,6 @@
 cp -rf /deploy-cfg/cctvsim/lib/* /opt/eva/python3/lib/python3.6/site-packages/ || exit 1
 cp -rf /deploy-cfg/cctvsim /opt || exit 1
 cp -f /deploy-cfg/cctvsim/cctvsim_supervisor.conf /etc/supervisor/conf.d/cctvsim.conf || exit 1
-( supervisorctl reread && supervisorctl add cctvsim && supervisorctl start cctvsim ) || exit 1
+( supervisorctl reread && supervisorctl add cctvsim ) || exit 1
 
 echo "OK"
