7f0233686e9569d5828fb977905eae2471215f68
divisor <div@altertech.com>
2019-07-09 16:18:48 +0200

    chart opts

--- ui/config/chart_options.js
+++ ui/config/chart_options.js
@@ -1,67 +1,73 @@
-var eva_hmi_config_chart_options = {
-  legend: {
-    display: false,
-  },
-  scales: {
-    yAxes: [{
-      ticks: {
-        fontSize: 14,
-        fontColor: '#999',
-        userCallback: function(value, index, values) {
-          if(index == 0 || index == values.length-1) return value;
-        },
-      },
-      gridLines: {
-        display: false,
-        lineWidth: 2,
-        tickMarkLength: 5,
-        color: "#999",
-      },
-    }],
-    xAxes: [{
-      type: 'time',
-      time: {
-        unit: 'hour',
-        unitStepSize: 1,
-        round: 'minute',
-        tooltipFormat: 'H:mm:ss',
-        displayFormats: {
-          hour: 'H:mm',
-        },
-      },
-      ticks: {
-        fontSize: 14,
-        fontColor: '#ccc',
-        maxTicksLimit: 12,
-        maxRotation: 0,
-        autoSkip: true,
-      },
-      gridLines: {
-        display: true,
-        lineWidth: 2,
-        color: "#999",
-        drawOnChartArea: false,
-        tickMarkLength: 10,
-        borderDash: [5, 10],
-      },
-    }],
-  },
-  plugins: {
-    filler: {
-      propagate: true
+function eva_hmi_config_chart_options() {
+  return {
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            fontSize: 14,
+            fontColor: '#999',
+            userCallback: function(value, index, values) {
+              if (index == 0 || index == values.length - 1) return value;
+            }
+          },
+          gridLines: {
+            display: false,
+            lineWidth: 2,
+            tickMarkLength: 5,
+            color: '#999'
+          }
+        }
+      ],
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            unit: 'hour',
+            unitStepSize: 1,
+            round: 'minute',
+            tooltipFormat: 'H:mm:ss',
+            displayFormats: {
+              hour: 'H:mm'
+            }
+          },
+          ticks: {
+            fontSize: 14,
+            fontColor: '#ccc',
+            maxTicksLimit: 12,
+            maxRotation: 0,
+            autoSkip: true
+          },
+          gridLines: {
+            display: true,
+            lineWidth: 2,
+            color: '#999',
+            drawOnChartArea: false,
+            tickMarkLength: 10,
+            borderDash: [5, 10]
+          }
+        }
+      ]
+    },
+    plugins: {
+      filler: {
+        propagate: true
+      }
+    },
+    elements: {
+      line: {
+        //tension: 0, // disables bezier curves
+        borderWidth: 2
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    },
+    animation: {
+      duration: 1000 // general animation time
     }
-  },
-  elements: {
-    line: {
-      //tension: 0, // disables bezier curves
-      borderWidth: 2,
-    }
-  },
-  tooltips: {
-    mode: 'index',
-    intersect: false,
-  },
-  animation: {
-    duration: 1000, // general animation time
-  },
-};
+  };
+}
