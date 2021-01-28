7980daeb5769f3d9c7e0186dd6810f21f5b95768
Sergei S <div@altertech.com>
2021-01-28 03:15:17 +0100

    auto setup/run

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 CCTV_PKG = [
-    'click==6.7',
-    'Flask==1.0.3',
-    'itsdangerous==1.1.0',
-    'Jinja2==2.11.2',
-    'MarkupSafe==1.1.1',
-    'Werkzeug==1.0.1',
-    'pillow==8.1.0',
-    'requests==2.21.0'
+    'click==6.7', 'Flask==1.0.3', 'itsdangerous==1.1.0', 'Jinja2==2.11.2',
+    'MarkupSafe==1.1.1', 'Werkzeug==1.0.1', 'pillow==8.1.0', 'requests==2.21.0'
 ]
 
 if event.type == CS_EVENT_PKG_INSTALL:
@@ -14,8 +8,33 @@ if event.type == CS_EVENT_PKG_INSTALL:
     extract_package()
     import venv
     import os
-    venv.create('/opt/eva/cctvsim/venv', with_pip=True)
-    code = os.system('/opt/eva/cctvsim/venv/bin/pip install ' +
+    venv.create(f'{dir_eva}/cctvsim/venv', with_pip=True)
+    code = os.system(f'{dir_eva}/cctvsim/venv/bin/pip install ' +
                      ' '.join(CCTV_PKG))
     if code:
         raise RuntimeError(f'pip failed with code {code}')
+    import subprocess
+    with open(f'{dir_eva}/xc/sfa/cs/cctvsim.py', 'w') as fh:
+        fh.write("""
+if event.type == CS_EVENT_SYSTEM:
+    if event.topic == 'startup':
+        import subprocess
+        g.cctvsim = subprocess.Popen(
+            [f'{dir_eva}/cctvsim/venv/bin/python',
+            f'{dir_eva}/cctvsim/cctvsim.py'])
+        logger.info('CCTV SIM started')
+    elif event.topic == 'shutdown':
+        try:
+            g.cctvsim.kill()
+            logger.info('CCTV SIM stopped')
+        except AttributeError:
+            pass
+""")
+    reload_corescripts(k=masterkey)
+    try:
+        g.cctvsim
+    except AttributeError:
+        g.cctvsim = subprocess.Popen([
+            f'{dir_eva}/cctvsim/venv/bin/python',
+            f'{dir_eva}/cctvsim/cctvsim.py'
+        ])
