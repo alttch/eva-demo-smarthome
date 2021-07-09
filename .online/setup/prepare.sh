bb9ea4894472bfb9cc8e1209ccb6981f3d5693c8
Sergei S <div@altertech.com>
2021-07-10 01:48:12 +0200

    disable logs

--- .online/setup/prepare.sh
+++ .online/setup/prepare.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -e
 
 echo "* * * * * /opt/sse/_sse.sh" >> /var/spool/cron/crontabs/root
+
+for c in uc lm sfa; do
+  AUTO_PREFIX=1 /opt/eva/sbin/eva-registry-cli set-field config/$c/main server/log-file /dev/null
+done
